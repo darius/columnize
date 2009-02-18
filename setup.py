8016ba2abd7e87b8f0d6955ea8a2197f3ee43da9
darius <darius@66-214-142-169.dhcp.gldl.ca.charter.com>
2009-02-18 13:58:17 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='columnize',
+      version='0.1.0',
+      description='View dir() output (or other strings) in columns.',
+      author='Darius Bacon',
+      author_email='darius@wry.me',
+      url='http://github.com/darius/columnize/tree/master',
+      py_modules=['columnize'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT X License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        ])
